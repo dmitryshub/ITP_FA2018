8201b57e793464c9c29bd472ca1896fe832bbfd7
dmitry.shub <dshub@colum.edu>
2018-10-31 20:48:53 -0500

    commit

---
+++ helloWorld_new/helloWorld_new/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace helloWorld_new
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string userInput;
+            Console.WriteLine("What is your name?");
+            userInput = Console.ReadLine();
+
+            Console.WriteLine("Hello World, " + userInput + "!");
+            Console.ReadLine();
+        }
+    }
+}
